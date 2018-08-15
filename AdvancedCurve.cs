6910cf84ec82a764b8db6174b69400d73a6e7f57
Karl <karolwieczorek9@o2.pl>
2018-08-15 14:26:17 +0200

    points to nodes

--- AdvancedCurve.cs
+++ AdvancedCurve.cs
@@ -7,8 +7,6 @@ namespace Hypnagogia.BezierCurve
 {
     public class AdvancedCurve : MonoBehaviour
     {
-        [SerializeField] Vector3[] points;
-
         [SerializeField] bool loop;
 
         [SerializeField] List<Node> nodes = new List<Node>();
@@ -22,61 +20,61 @@ namespace Hypnagogia.BezierCurve
                 if (value == true)
                 {
                     nodes[nodes.Count - 1].mode = nodes[0].mode;
-                    SetControlPoint(0, points[0]);
+                    SetControlPoint(0, GetPosition(0));
                 }
             }
         }
 
         public int ControlPointCount
         {
-            get { return points.Length; }
+            get { return nodes.Count * 3 - 2; }
         }
 
         public Vector3 GetControlPoint(int index)
         {
-            return points[index];
+            return GetPosition(index);
         }
 
         public void SetControlPoint(int index, Vector3 point)
         {
             if (index % 3 == 0)
             {
-                Vector3 delta = point - points[index];
+                Vector3 delta = point - GetPosition(index);
                 if (loop)
                 {
                     if (index == 0)
                     {
-                        points[1] += delta;
-                        points[points.Length - 2] += delta;
-                        points[points.Length - 1] = point;
+                        MovePosition(1, delta);
+                        MovePosition(ControlPointCount - 2, delta);
+                        SetPosition(ControlPointCount - 1, point);
                     }
-                    else if (index == points.Length - 1)
+                    else if (index == ControlPointCount - 1)
                     {
-                        points[0] = point;
-                        points[1] += delta;
-                        points[index - 1] += delta;
+                        SetPosition(0, point);
+                        MovePosition(1, delta);
+                        MovePosition(index - 1, delta);
                     }
                     else
                     {
-                        points[index - 1] += delta;
-                        points[index + 1] += delta;
+                        MovePosition(index - 1, delta);
+                        MovePosition(index + 1, delta);
                     }
                 }
                 else
                 {
                     if (index > 0)
                     {
-                        points[index - 1] += delta;
+                        MovePosition(index - 1, delta);
                     }
 
-                    if (index + 1 < points.Length)
+                    if (index + 1 < ControlPointCount)
                     {
-                        points[index + 1] += delta;
+                        MovePosition(index + 1, delta);
                     }
                 }
             }
 
-            points[index] = point;
+            SetPosition(index, point);
             EnforceMode(index);
         }
 
@@ -114,12 +112,17 @@ namespace Hypnagogia.BezierCurve
             
             if (positionIndex == 0)
                 node.point = position;
-            if (positionIndex == 1)
+            else if (positionIndex == 1)
                 node.controlPoint0 = position;
-            if (positionIndex == -1)
+            else if (positionIndex == -1)
                 node.controlPoint1 = position;
-            
-            throw new Exception("node out of range");
+            else
+                throw new Exception("node out of range");
+        }
+
+        void MovePosition(int index, Vector3 moveAmount)
+        {
+            SetPosition(index, GetPosition(index) + moveAmount);
         }
 
         public void SetControlPointMode(int index, BezierControlPointMode mode)
@@ -152,11 +155,11 @@ namespace Hypnagogia.BezierCurve
                 fixedIndex = middleIndex - 1;
                 if (fixedIndex < 0)
                 {
-                    fixedIndex = points.Length - 2;
+                    fixedIndex = ControlPointCount - 2;
                 }
 
                 enforcedIndex = middleIndex + 1;
-                if (enforcedIndex >= points.Length)
+                if (enforcedIndex >= ControlPointCount)
                 {
                     enforcedIndex = 1;
                 }
@@ -164,7 +167,7 @@ namespace Hypnagogia.BezierCurve
             else
             {
                 fixedIndex = middleIndex + 1;
-                if (fixedIndex >= points.Length)
+                if (fixedIndex >= ControlPointCount)
                 {
                     fixedIndex = 1;
                 }
@@ -172,23 +175,23 @@ namespace Hypnagogia.BezierCurve
                 enforcedIndex = middleIndex - 1;
                 if (enforcedIndex < 0)
                 {
-                    enforcedIndex = points.Length - 2;
+                    enforcedIndex = ControlPointCount - 2;
                 }
             }
 
-            Vector3 middle = points[middleIndex];
-            Vector3 enforcedTangent = middle - points[fixedIndex];
+            Vector3 middle = GetPosition(middleIndex);
+            Vector3 enforcedTangent = middle - GetPosition(fixedIndex);
             if (mode == BezierControlPointMode.Aligned)
             {
-                enforcedTangent = enforcedTangent.normalized * Vector3.Distance(middle, points[enforcedIndex]);
+                enforcedTangent = enforcedTangent.normalized * Vector3.Distance(middle, GetPosition(enforcedIndex));
             }
 
-            points[enforcedIndex] = middle + enforcedTangent;
+            SetPosition(enforcedIndex, middle + enforcedTangent);
         }
 
         public int CurveCount
         {
-            get { return (points.Length - 1) / 3; }
+            get { return (ControlPointCount - 1) / 3; }
         }
 
         public Vector3 GetPoint(float t)
@@ -197,7 +200,7 @@ namespace Hypnagogia.BezierCurve
             if (t >= 1f)
             {
                 t = 1f;
-                i = points.Length - 4;
+                i = ControlPointCount - 4;
             }
             else
             {
@@ -207,7 +210,7 @@ namespace Hypnagogia.BezierCurve
                 i *= 3;
             }
 
-            return transform.TransformPoint(Bezier.GetPoint(points[i], points[i + 1], points[i + 2], points[i + 3], t));
+            return transform.TransformPoint(Bezier.GetPoint(GetPosition(i), GetPosition(i + 1), GetPosition(i + 2), GetPosition(i + 3), t));
         }
 
         public Vector3 GetVelocity(float t)
@@ -216,7 +219,7 @@ namespace Hypnagogia.BezierCurve
             if (t >= 1f)
             {
                 t = 1f;
-                i = points.Length - 4;
+                i = ControlPointCount - 4;
             }
             else
             {
@@ -226,8 +229,8 @@ namespace Hypnagogia.BezierCurve
                 i *= 3;
             }
 
-            return transform.TransformPoint(Bezier.GetFirstDerivative(points[i], points[i + 1], points[i + 2],
-                       points[i + 3], t)) - transform.position;
+            return transform.TransformPoint(Bezier.GetFirstDerivative(GetPosition(i), GetPosition(i + 1), GetPosition(i + 2),
+                       GetPosition(i + 3), t)) - transform.position;
         }
 
         public Vector3 GetDirection(float t)
@@ -238,21 +241,20 @@ namespace Hypnagogia.BezierCurve
         public void AddCurve()
         {
             nodes.Add(new Node());
-            Vector3 point = points[points.Length - 1];
-            Array.Resize(ref points, points.Length + 3);
+            Vector3 point = GetPosition(ControlPointCount - 1);
             point.x += 1f;
-            points[points.Length - 3] = point;
+            SetPosition(ControlPointCount - 3, point);
             point.x += 1f;
-            points[points.Length - 2] = point;
+            SetPosition(ControlPointCount - 2, point);
             point.x += 1f;
-            points[points.Length - 1] = point;
+            SetPosition(ControlPointCount - 1, point);
 
             nodes[nodes.Count - 1].mode = nodes[nodes.Count - 2].mode;
-            EnforceMode(points.Length - 4);
+            EnforceMode(ControlPointCount - 4);
 
             if (loop)
             {
-                points[points.Length - 1] = points[0];
+                SetPosition(ControlPointCount - 1, GetPosition(0));
                 nodes[nodes.Count - 1].mode = nodes[0].mode;
                 EnforceMode(0);
             }
@@ -260,14 +262,6 @@ namespace Hypnagogia.BezierCurve
 
         public void Reset()
         {
-            points = new Vector3[]
-            {
-                new Vector3(1f, 0f, 0f),
-                new Vector3(2f, 0f, 0f),
-                new Vector3(3f, 0f, 0f),
-                new Vector3(4f, 0f, 0f)
-            };
-
             nodes = new List<Node>()
             {
                 new Node(),
